93f9c4447504c1b56071450724f753de339465d9
Hiago <hiagofml11@gmail.com>
2026-09-05 00:44:59 -0300

    fix: Concertando a porra do teste

--- src/components/ui/Taskbar.tsx
+++ src/components/ui/Taskbar.tsx
@@ -62,6 +62,7 @@ export default function Taskbar() {
             </div>
 
             <time
+                suppressHydrationWarning
                 className="
           text-[10px]
           font-bold
